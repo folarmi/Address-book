38a8bb31ce82cd73b5d5e85a7e0bf048bd0680a7
folarmi <fola4ril@gmail.com>
2020-05-16 13:16:38 -0700

    user can delete contact

--- app.js
+++ app.js
@@ -9,24 +9,9 @@ const vm = new Vue ({
         lastName:'',
         phoneNumber:'',
         email:'',
-        isActive:false,
-    // columns: ['F.name', 'L.Name', 'PhoneNumber','email'],   
-    contacts: [
-      {
-        id: 1,
-        firstName: 'Smith',
-        lastName: 'John',
-        phoneNumber: '167876578',
-        email: 'fola4ril@gmail.com'
-      },
-      {
-        id: 2,
-        firstName: 'Smith',
-        lastName: 'Mary',
-        phoneNumber: '567890',
-        email: 'fola4ril@gmail.com'
-      }
-    ]
+        show:true,
+
+    contacts: []
   }
     },
     methods: {
@@ -37,21 +22,16 @@ const vm = new Vue ({
                 phoneNumber:this.phoneNumber,
                 email:this.email,
               }) 
-              var spanElement = document.createElement("span");
-              
-              var icon = document.createElement("i");
-              const test2 = icon.classList.add('fas', 'fa-times');
-              console.log(test2)
-              spanElement.append(icon);
-              const test = ul.appendChild(li).append(spanElement)
-              console.log(test)
-
+ 
               this.contacts.push(newContact);
               this.firstName = '';          
               this.lastName = '';          
               this.phoneNumber = '';          
               this.email = ''; 
               this.isActive = true;  
+        },
+        close(index) {
+          this.contacts.splice(index,1);
         }
     }
 })
